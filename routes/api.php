90b3ec2cd94c8eae5dcbc72b4951d1ca443d6cff
zloyleva <zloyleva@gmail.com>
2019-01-15 23:32:26 +0200

    add routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/repo', 'GithubAPIController@index');
+Route::get('/repo/{owner}/{repo}', 'GithubAPIController@show');
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('repo/{owner}/{repo}', 'HomeController@show')->name('show');
